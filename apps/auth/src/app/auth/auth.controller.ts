6beb7275ed92fa1209393af00df14c0f93cbcef2
Topor <topordon@gmail.com>
2020-12-02 16:53:24 +0300

    [fix] restore files;

---
+++ apps/auth/src/app/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, OnModuleInit } from '@nestjs/common';
+import {
+  Client,
+  ClientGrpc,
+  GrpcMethod,
+  Transport,
+} from '@nestjs/microservices';
+import { Metadata, ServerUnaryCall } from 'grpc';
+
+import { userGrpc, UserServiceClient } from '@api/core';
+import { environment } from '@api/env-auth';
+
+@Controller()
+export class AuthController implements OnModuleInit {
+
+  @Client(userGrpc(Transport.GRPC, environment.port.toString()))
+  private readonly userClientGrpc: ClientGrpc;
+  private userServiceGrps: UserServiceClient;
+
+  constructor() {}
+
+  public onModuleInit() {
+    this.userServiceGrps = this.userClientGrpc.getService('UserService');
+  }
+
+//   @GrpcMethod('AuthService', 'auth')
+//   auth(data: ReqAuth,  metadata: Metadata, call: ServerUnaryCall<any>): ResAuth {
+//     const items = [
+//       { email: '', password: 'John' },
+//       { email: '', password: 'Doe' },
+//     ];
+//     return items.find(({ id }) => id === data.id);
+//   }
+}
