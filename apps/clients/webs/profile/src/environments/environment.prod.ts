dbb222d4c8dfce6e81b7f76069cb192369813a6b
kenobi io <io.kenobi@gmail.com>
2022-05-31 13:40:31 +0300

    [fix] remove profile web;

--- apps/clients/webs/profile/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-import { Environment, prefix, port, inc } from '@ngn-template/api-interfaces';
-
-const { global, bff } = prefix;
-
-export const environment: Environment = {
-  production: true,
-  prefix: `${global}/${bff}`,
-  port: port + inc.bff,
-};
